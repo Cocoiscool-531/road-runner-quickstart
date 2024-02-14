9c1146cf0a5277b3a792ea6c00a004741a235f2e
Cocoiscool-531 <119710842+Cocoiscool-531@users.noreply.github.com>
2024-02-14 18:04:17 -0500

    Conversion to 0.5

---
+++ MeepMeep/src/main/java/com/example/meepmeep/MeepMeepRun.java
@@ -0,0 +1,40 @@
+package com.example.meepmeep;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class MeepMeepRun {
+    public static void main(String[] args) {
+
+        double MAX_VEL = 54.66855022514893;
+        double MAX_ACCEL = 54.66855022514893;
+        double MAX_ANG_VEL = Math.toRadians(174.0154);
+        double MAX_ANG_ACCEL = Math.toRadians(174.0154);
+
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(MAX_VEL, MAX_ACCEL, MAX_ANG_VEL, MAX_ANG_ACCEL, 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
+                                .forward(30)
+                                .turn(Math.toRadians(90))
+                                .forward(30)
+                                .turn(Math.toRadians(90))
+                                .forward(30)
+                                .turn(Math.toRadians(90))
+                                .forward(30)
+                                .turn(Math.toRadians(90))
+                                .build()
+                );
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
